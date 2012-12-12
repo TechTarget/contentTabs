b0a31bdf9e7b0fd7113549073bbb594d5050efaf
Morgan Wigmanich <okize123@gmail.com>
2012-12-12 16:35:31 -0500

    initial commit

---
+++ Makefile
@@ -0,0 +1,33 @@
+# LINT & MINIFY
+# jshint (>=0.7.0) & uglifyjs (>=1.2.6) are required
+
+SCRIPT_DEV = contentTabs.js
+SCRIPT_MIN = contentTabs.min.js
+
+FILESIZE_MAX = 1000
+FILESIZE_GZIP = `gzip -c ${SCRIPT_MIN} | wc -c`
+FILESIZE_PASS = "${FILESIZE_GZIP} bytes  \(^_^)/"
+FILESIZE_FAIL = "${FILESIZE_GZIP} bytes  ^(>_<)^"
+
+define FILESIZE_CHECK
+	if [ ${FILESIZE_GZIP} -gt ${FILESIZE_MAX} ]; then \
+		tput setaf 1; \
+		echo ${FILESIZE_FAIL}; \
+		tput sgr0; \
+    else \
+		tput setaf 2; \
+		echo ${FILESIZE_PASS}; \
+		tput sgr0; \
+	fi
+endef
+
+default:
+
+	@echo "* linting..."
+	@jshint ${SCRIPT_DEV} --show-non-errors
+
+	@echo "* minifying..."
+	@uglifyjs ${SCRIPT_DEV} > ${SCRIPT_MIN}
+
+	@echo "* gzip test..."
+	@$(FILESIZE_CHECK)
---
+++ contentTabs.js
@@ -0,0 +1,122 @@
+/*!
+* Content Tabs v0.1 (http://okize.github.com/)
+* Copyright (c) 2012 | Licensed under the MIT license - http://www.opensource.org/licenses/mit-license.php
+*/
+
+;(function ($) {
+
+    'use strict';
+
+    $.fn.tabList = function (options) {
+
+        var defaults = {
+            tabContent: '',
+            icon: true,
+            callback: function () {},
+            mouseEvent: 'click',
+            autoPlay: false,
+            autoPlaySpeed: 5000
+        };
+        var o = $.extend(defaults, options);
+
+        return this.each(function () {
+
+            var tabs = $(this).children('li');
+            var tabsCount = tabs.length;
+            var tabLinks = tabs.children('a');
+            var tabContent = o.tabContent;
+            var currentTab = 0;
+            var selectedTab;
+            var timer;
+
+            // do tabby stuff
+            var tabify = function (el) {
+
+                selectTab(el);
+                showContent(String(el.hash));
+
+                // optional callback function
+                o.callback.call(this);
+
+            };
+
+            // add 'selected' class to new tab and remove it from old tab
+            var selectTab = function (el) {
+
+                tabs.filter('.selected').removeClass('selected');
+                $(el).parent('li').addClass('selected');
+
+            };
+
+            // if one of the tab links has a hash of 'all' then display every piece of content, otherwise display the selected tabs content
+            var showContent = function (tabHash) {
+
+                if (tabHash === '#all') {
+                    tabContent.show();
+                } else {
+                    tabContent.hide().filter(tabHash).fadeIn('fast');
+                }
+
+            };
+
+            // timer function for autoplay
+            var autoPlayTimer = function () {
+                timer = setTimeout(function () {
+                    autoPlay();
+                }, o.autoPlaySpeed);
+            };
+
+            // check which tab is selected and select next sibling
+            var autoPlay = function () {
+
+                if (currentTab < tabsCount - 1) {
+                    currentTab++;
+                } else {
+                    currentTab = 0;
+                }
+                tabify(tabLinks.get(currentTab));
+                autoPlayTimer();
+
+            };
+
+            // autoplay init
+            if (o.autoPlay) {
+                if (tabsCount > 1) {
+                    autoPlayTimer();
+                }
+            }
+
+            // event handler for tabs
+            tabLinks.on(o.mouseEvent, function (e) {
+
+                e.preventDefault();
+                tabify(this);
+
+                // if autoplay is enabled, restart it
+                if (o.autoPlay) {
+                    clearTimeout(timer);
+                    autoPlayTimer();
+                    currentTab = tabLinks.index(this);
+                }
+
+            });
+
+            // add a span element if icon option set
+            if (o.icon) {
+                tabLinks.append('<span></span>');
+            }
+
+            // check if any of the tabs have a selected class and if so, display that tab content; otherwise select the first tab
+            if (tabs.hasClass('selected')) {
+                selectedTab = tabs.filter('.selected').children('a');
+                currentTab = tabLinks.index(selectedTab);
+                showContent(selectedTab.attr('href'));
+            } else {
+                tabs.first().addClass('selected');
+            }
+
+        });
+
+    };
+
+})(jQuery);
