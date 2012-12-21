19f324097f40e92cbdb622fe1ab812874671ac12
Morgan Wigmanich <okize123@gmail.com>
2012-12-21 12:25:10 -0500

    removed file

--- contentTabs.js
+++ contentTabs.js
@@ -6,54 +6,50 @@
 // use AMD or browser globals to create a jQuery plugin.
 ;(function (factory) {
 
-    if (typeof define === 'function' && define.amd) {
-        define(['jquery'], factory);
-    } else {
-        factory(jQuery);
-    }
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery'], factory);
+  } else {
+    factory(jQuery);
+  }
 
 }(function ($) {
 
-    // defaults
-    var pluginName = 'contentTabs',
-        defaults = {
-            tabActiveClass: 'active',
-            panelActiveClass: 'active',
-            mouseEvent: 'click'
-        };
+  'use strict';
 
-    // plugin constructor
-    function Plugin( element, options ) {
-        this.element = element;
-        this.options = $.extend( {}, defaults, options) ;
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
+  // defaults
+  var pluginName = 'contentTabs';
+  var defaults = {
+    tabActiveClass: 'active',
+    panelActiveClass: 'active',
+    mouseEvent: 'click'
+  };
+
+  // plugin constructor
+  function Plugin( element, options ) {
+    this.element = element;
+    this.options = $.extend( {}, defaults, options) ;
+    this._defaults = defaults;
+    this._name = pluginName;
+    this.init();
+  }
+
+  Plugin.prototype = {
+
+    init: function() {
+
     }
 
-    Plugin.prototype = {
+  };
 
-        tabs: $('.contentTabsNav', this.element).find('li'),
-
-        init: function() {
-          console.log(this.tabs);
-        },
-
-        selectTab: function(i) {
-          alert(i);
-        }
-
-    };
-
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName,
-                new Plugin( this, options ));
-            }
-        });
-    }
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[pluginName] = function ( options ) {
+    return this.each(function () {
+      if (!$.data(this, 'plugin_' + pluginName)) {
+        $.data(this, 'plugin_' + pluginName,
+          new Plugin( this, options ));
+      }
+    });
+  };
 
 }));
